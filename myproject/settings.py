dee0d6b7415cdad109c894c455ec637a1c88fd60
Seyran <269787076+Seyran6@users.noreply.github.com>
2026-05-18 19:05:33 +0300

    commit 3

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'default-key-if-not-found')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
